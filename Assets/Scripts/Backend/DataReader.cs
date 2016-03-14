dc4a1104c82ba2aa49a96fafe6a1106d858831c1
gfox <gfox@backflipstudios.com>
2016-03-14 11:00:44 -0700

    Fixing json formatting.

--- Assets/Scripts/Backend/DataReader.cs
+++ Assets/Scripts/Backend/DataReader.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
 using Newtonsoft.Json;
+
+using UnityEngine;
 
 namespace BDChef
 {
@@ -7,16 +10,19 @@ namespace BDChef
 	{
 		public static Book ReadBook(string itemFile, string recipeFile)
 		{
-			// READ IN DATA
-			Book ofTheKing = new Book(null, null);
-
 			var itemText = Resources.Load(itemFile) as TextAsset;
 			Utils.NullAsset(itemText, "Couldn't load resource: " + itemFile);
 
 			var recipeText = Resources.Load(recipeFile) as TextAsset;
 			Utils.NullAsset(recipeText, "Couldn't load resource: " + recipeFile);
 
-			return ofTheKing;
+			return new Book(Parse<Item>(itemText), Parse<Recipe>(recipeText));
+		}
+
+		private static List<T> Parse<T>(string stringData)
+		{
+			var data = JsonConvert.DeserializeObject<List<T>>(stringData);
+			return data;
 		}
 	}
 }
