a5bb80d751310002de6a60d6eef15f98cb01b369
gfox <gfox@backflipstudios.com>
2016-03-14 09:42:46 -0700

    Adding some Utilities.

--- Assets/Scripts/Backend/DataReader.cs
+++ Assets/Scripts/Backend/DataReader.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using UnityEngine;
+using Newtonsoft.Json;
 
 namespace BDChef
 {
@@ -7,8 +8,15 @@ namespace BDChef
 		public static Book ReadBook(string itemFile, string recipeFile)
 		{
 			// READ IN DATA
-
 			Book ofTheKing = new Book(null, null);
+
+			var itemText = Resources.Load(itemFile) as TextAsset;
+			Utils.NullAsset(itemText, "Couldn't load resource: " + itemFile);
+
+			var recipeText = Resources.Load(recipeFile) as TextAsset;
+			Utils.NullAsset(recipeText, "Couldn't load resource: " + recipeFile);
+
+			return ofTheKing;
 		}
 	}
 }
---
+++ Assets/Scripts/Utility/Utils.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace BDChef
+{
+	public static class Utils
+	{
+		public static void NullAsset(System.Object obj, string message = null)
+		{
+			Assert(obj != null, message);
+		}
+
+		public static void Assert(bool condition, string message = null)
+		{
+			if (!condition)
+			{
+				if (message == null)
+				{
+					message = "Assert Failed";
+				}
+
+				Debug.LogError(message);
+			}
+		}
+	}
+}
